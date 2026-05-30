dc2a6203b5ced44becab110d6b065b6a78d5590d
abdallahmehanna11136-collab <abdallahmehanna11136@gmail.com>
2026-05-31 00:34:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, send
-import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'abdo_secret_nile_key'
+# تشغيل عادي وبسيط متوافق مع ريندر مباشرة
 socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
@@ -14,6 +14,11 @@ def index():
 def handle_message(msg):
     send(msg, broadcast=True)
 
+@socketio.on('user_join')
+def handle_user_join(username):
+    send(f"📢 نظام نايل شات: {username} انضم إلى الغرفة الآن!", broadcast=True)
+
 if __name__ == '__main__':
+    import os
     port = int(os.environ.get('PORT', 5000))
     socketio.run(app, host='0.0.0.0', port=port)
