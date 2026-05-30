34b2075798370142b8775bcf0ad252ddb6cd0fb9
abdallahmehanna11136-collab <abdallahmehanna11136@gmail.com>
2026-05-30 23:11:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,8 @@ def index():
 
 @socketio.on('message')
 def handle_message(msg):
+    # السيرفر بياخد الرسالة ويبعتها لكل الناس فوراً
     send(msg, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000)
